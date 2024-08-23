6c38adad6b4537c107c83bbe3d27a97e4240b3eb
sainathreddy-143 <peddireddysainathreddy143@gmail.com>
2024-08-23 07:50:37 +0530

    Create App.js

---
+++ client/public/src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import BookingForm from './components/BookingForm';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>CareerCarve Scheduler</h1>
+            <BookingForm />
+        </div>
+    );
+}
+
+export default App;
